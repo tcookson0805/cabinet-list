c9d5ff2fb123c907df544dc4a417b74df216af3e
Tyler Cookson <tcookson0805@gmail.com>
2016-06-17 13:05:34 -0700

    list items options now slide up and down on doubleclick

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+
+  
+  $('.aside-left li').on('dblclick', function(){
+    var options = $(this).children()
+    if(options.is(':hidden')){
+      options.slideDown('slow')  
+    }else{
+      options.slideUp('slow');
+    }
+  })  
+  
+  $('.aside-right li').on('click', function(){
+    var options = $(this).children()
+    if(options.is(':hidden')){
+      options.slideDown('slow')  
+    }else{
+      options.slideUp('slow');
+    }
+  })
+  
+});
