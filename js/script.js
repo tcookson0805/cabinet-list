750fe9c7dad9097745a5e0c33c9059f9c0542b73
Tyler Cookson <tcookson0805@gmail.com>
2016-06-18 17:15:04 -0700

    adding fake text to list

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 $(document).ready(function(){
 
-
+  $('.aside-left').on('click', 'li', function(){
+    $(this).toggleClass('highlight')
+  });
   
-  $('.aside-left li').on('dblclick', function(){
-    var options = $(this).children()
-    if(options.is(':hidden')){
-      options.slideDown('slow')  
-    }else{
-      options.slideUp('slow');
-    }
-  })  
+  $('.aside-right').on('click', 'li', function(){
+    $(this).toggleClass('highlight')
+  });
   
-  $('.aside-right li').on('click', function(){
-    var options = $(this).children()
-    if(options.is(':hidden')){
-      options.slideDown('slow')  
+  $('.arrow').on('click', function(){
+    $('.aside-left li.highlight').appendTo('.aside-right ul').removeClass('highlight');
+    $('.aside-right li.highlight').appendTo('.aside-left ul').removeClass('highlight');
+  });
+  
+  $('.aside-left').on('dblclick', 'li', function(){
+    if($(this).children().is(':hidden')){
+      $(this).children().slideDown('slow');
     }else{
-      options.slideUp('slow');
+      $(this).children().slideUp('slow')
     }
-  })
+  });  
+  
+  $('.aside-right').on('dblclick', 'li', function(){
+    if($(this).children().is(':hidden')){
+      $(this).children().slideDown('slow');
+    }else{
+      $(this).children().slideUp('slow')
+    }
+  });
+  
+  $('button').click( function(){
+    var $input = $('.input-text').val();
+    $('.to-get-items').append('<div>asdfasd</div>')   
+  });
+  
   
 });
