cd02bd1a5963040fa68a7a7db6804cb095e257c1
Tyler Cookson <tcookson0805@gmail.com>
2016-06-22 13:40:52 -0700

    made website responsive

--- js/script.js
+++ js/script.js
@@ -2,32 +2,44 @@ $(document).ready(function(){
 
   $('.aside-left').on('click', 'li', function(){
     $(this).toggleClass('highlight')
+    
+    if($(this).children('.options').is(':hidden')){
+      $(this).children('.options').slideDown('slow');
+    }else{
+      $(this).children('.options').slideUp('slow')
+    }
   });
   
   $('.aside-right').on('click', 'li', function(){
     $(this).toggleClass('highlight')
+    
+    if($(this).children('.options').is(':hidden')){
+      $(this).children('.options').slideDown('slow');
+    }else{
+      $(this).children('.options').slideUp('slow')
+    }
   });
   
   $('.arrow').on('click', function(){
-    $('.aside-left li.highlight').appendTo('.aside-right ul').removeClass('highlight');
-    $('.aside-right li.highlight').appendTo('.aside-left ul').removeClass('highlight');
+    $('.aside-left li.highlight').appendTo('.aside-right ul').removeClass('highlight').children('.options').hide();
+    $('.aside-right li.highlight').appendTo('.aside-left ul').removeClass('highlight').children('.options').hide()
   });
   
-  $('.aside-left').on('dblclick', 'li', function(){
-    if($(this).children('.options').is(':hidden')){
-      $(this).children('.options').slideDown('slow');
-    }else{
-      $(this).children('.options').slideUp('slow')
-    }
-  });  
+  // $('.aside-left').on('dblclick', 'li', function(){
+  //   if($(this).children('.options').is(':hidden')){
+  //     $(this).children('.options').slideDown('slow');
+  //   }else{
+  //     $(this).children('.options').slideUp('slow')
+  //   }
+  // });  
   
-  $('.aside-right').on('dblclick', 'li', function(){
-    if($(this).children('.options').is(':hidden')){
-      $(this).children('.options').slideDown('slow');
-    }else{
-      $(this).children('.options').slideUp('slow')
-    }
-  });
+  // $('.aside-right').on('dblclick', 'li', function(){
+  //   if($(this).children('.options').is(':hidden')){
+  //     $(this).children('.options').slideDown('slow');
+  //   }else{
+  //     $(this).children('.options').slideUp('slow')
+  //   }
+  // });
   
   $('#input-form').on('click', '.add', function(){
     var $input = $('.input-text').val();
@@ -35,12 +47,6 @@ $(document).ready(function(){
     $('.to-get-items').append('<li class="item"><div class="something">' + $input + '</div><div class="edit-box"><form class="edit-box-text" onSubmit="return false"><input type="text" value="" class="edit-text"></form></div><div class="options"><div class="trash"><i class="fa fa-trash-o" aria-hidden="true"></i></div><div class="edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></div></div></li>')   
     $('.input-text').val('');
   });
-  
-    
-  $('.hero').on('click', '.trash', function(){
-    $(this).closest('.item').remove();
-  });
-  
   
   $('.hero').on('click', '.edit', function(){
     var $item = $(this).parent().siblings('.something').text()
@@ -50,11 +56,19 @@ $(document).ready(function(){
 
   });
   
+  $('.hero').on('click', '.trash', function(){
+    $(this).closest('.item').remove();
+  });
+  
+  $('.hero').on('click', '.move', function(){
+    $('.aside-left li.highlight').appendTo('.aside-right ul').removeClass('highlight').children('.options').hide();
+    $('.aside-right li.highlight').appendTo('.aside-left ul').removeClass('highlight').children('.options').hide();
+  });
+  
   
   $('.hero').on('keydown', '.edit-text',  function(e){
     var $changedItem = $(this).parent().parent().siblings('.something')
     var $newText = $(this).val();
-
     
     if(e.keyCode == 13){
       
