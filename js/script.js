4af0392fbf822b96823b9dff96fdbd0b9089819c
Tyler Cookson <tcookson0805@gmail.com>
2016-06-22 11:12:50 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ $(document).ready(function(){
   
   $('#input-form').on('click', '.add', function(){
     var $input = $('.input-text').val();
-    
     $('.to-get-items').append('<li class="item"><div class="something">' + $input + '</div><div class="edit-box"><form class="edit-box-text" onSubmit="return false"><input type="text" value="" class="edit-text"></form></div><div class="options"><div class="trash"><i class="fa fa-trash-o" aria-hidden="true"></i></div><div class="edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></div></div></li>')   
     $('.input-text').val('');
   });
@@ -47,25 +46,29 @@ $(document).ready(function(){
     $(this).parent().siblings('.something').hide();
     $(this).parent().siblings('.edit-box').children('.edit-box-text').children('.edit-text').attr('value', $item);
     $(this).parent().siblings('.edit-box').show();
-
   });
   
   
   $('.hero').on('keydown', '.edit-text',  function(e){
     var $changedItem = $(this).parent().parent().siblings('.something')
-    var $newText = $(this).val();
-
-    
+    var $newText = $(this).val()
     if(e.keyCode == 13){
-      
       $changedItem.text($newText);
       $changedItem.show();
       $(this).parent().parent().hide();
       $(this).parent().parent().siblings('.options').slideUp('fast');
     }
-    
-  })
+  });
 
+  $('#input-form').on('keydown', '.input-text', function(e){
+    var $input = $('.input-text').val();
+
+    if(e.keyCode == 13){
+      e.preventDefault();
+      $('.to-get-items').append('<li class="item"><div class="something">' + $input + '</div><div class="edit-box"><form class="edit-box-text" onSubmit="return false"><input type="text" value="" class="edit-text"></form></div><div class="options"><div class="trash"><i class="fa fa-trash-o" aria-hidden="true"></i></div><div class="edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></div></div></li>')   
+      $('.input-text').val(''); 
+    }
+  })
   
 });
 
